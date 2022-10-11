99b91567c9461ff07cefc8023bbc0022f08a34fc
IrynaHavryliuk <99551348+IrynaHavryliuk@users.noreply.github.com>
2022-10-11 12:32:46 +0300

    add

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -2,3 +2,53 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector('.gallery');
+const galleryMarkup = createMarkup(galleryItems);
+console.log(galleryMarkup);
+
+gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+gallery.addEventListener('click', onGalleryContainerClick);
+
+function createMarkup(galleryItems) {
+    return galleryItems
+        .map(({ preview, original, description }) => {
+            return `
+                <div class="gallery__item">
+                    <a class="gallery__link" href="${preview}">
+                        <img
+                            class="gallery__image"
+                            src="${preview}"
+                            data-source="${original}"
+                            alt="${description}"
+                        />
+                    </a>
+                </div>
+            `;
+        })
+        .join('');
+}
+
+function onGalleryContainerClick(event) {
+    event.preventDefault();
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const instance = basicLightbox.create(
+        `<img src="${event.target.dataset.source}" width="800" height="600">`
+    );
+    instance.show();
+
+    window.addEventListener('keydown', onEscPress);
+
+    function onEscPress(event) {
+        const Esc_code = 'Escape';
+        const isEscPress = event.code === Esc_code;
+
+        if (isEscPress) {
+            instance.close();
+        }
+    }
+}
